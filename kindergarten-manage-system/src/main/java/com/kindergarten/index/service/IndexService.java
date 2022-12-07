446bb5fc65ee0fae56bd2788c195a495490bb6df
王瑞玉 <wangruiyu@bjknrt.com>
2022-12-07 10:47:55 +0800

    feat:添加首页接口

---
+++ kindergarten-manage-system/src/main/java/com/kindergarten/index/service/IndexService.java
@@ -0,0 +1,7 @@
+package com.kindergarten.index.service;
+
+import org.springframework.stereotype.Service;
+
+
+public interface IndexService {
+}
---
+++ kindergarten-manage-system/src/main/java/com/kindergarten/index/service/impl/IndexServiceImpl.java
@@ -0,0 +1,8 @@
+package com.kindergarten.index.service.impl;
+
+import com.kindergarten.index.service.IndexService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndexServiceImpl implements IndexService {
+}
