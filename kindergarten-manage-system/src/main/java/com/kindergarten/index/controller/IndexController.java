fe78e2a478d80804192b22ee1d4d38b58206c32b
zzsgy <1173303436@qq.com>
2022-12-09 20:47:14 +0800

    remove unused tested interface
    1. 去除用来测试的无用的接口
    
    Signed-off-by: zzsgy <1173303436@qq.com>

--- kindergarten-manage-system/src/main/java/com/kindergarten/index/controller/IndexController.java
+++ kindergarten-manage-system/src/main/java/com/kindergarten/index/controller/IndexController.java
@@ -1,17 +1,12 @@
 package com.kindergarten.index.controller;
 
-import com.kindergarten.basic.exception.KindergartenException;
 import com.kindergarten.index.service.IndexService;
 import com.kindergarten.index.vo.StatsCountVO;
 import com.kindergarten.recipe.entity.Recipe;
-import com.kindergarten.student.entity.Student;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,37 +15,6 @@ public class IndexController {
 
     @Resource
     private IndexService indexService;
-
-    @RequestMapping("/e1")
-    public Object e1() {
-         throw new KindergartenException("XXX");
-    }
-    @RequestMapping("/e2")
-    public Object e2a() {
-       throw new RuntimeException("xxxaaa");
-    }
-    @RequestMapping("/num")
-    public Integer a() {
-        return 10;
-    }
-
-    @RequestMapping(
-            value = "/str",
-            produces = {MediaType.APPLICATION_JSON_VALUE}
-    )
-    public String str() {
-        return "lalalala";
-    }
-
-    @RequestMapping("/obj")
-    public Object obj() {
-        return new Student();
-    }
-
-    @RequestMapping("/list")
-    public List<Object> list() {
-        return Collections.emptyList();
-    }
 
     @RequestMapping("/getStatsCount")
     public StatsCountVO getStatsCount() {
