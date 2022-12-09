6fd7829f11db41e4a7523fc22282e19d6a238730
wangruiyu <wry10150@163.com>
2022-12-10 07:58:28 +0800

    fix:启用mybatisplus

--- kindergarten-manage-system/src/main/java/com/kindergarten/KindergartenManageSystemApplication.java
+++ kindergarten-manage-system/src/main/java/com/kindergarten/KindergartenManageSystemApplication.java
@@ -1,11 +1,12 @@
 package com.kindergarten;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.kindergarten.manage.system.mapper")
+@MapperScan("com.kindergarten.*.mapper")
 @Slf4j
 public class KindergartenManageSystemApplication {
 
--- kindergarten-manage-system/src/main/java/com/kindergarten/basic/config/MybatisPlusConfig.java
+++ kindergarten-manage-system/src/main/java/com/kindergarten/basic/config/MybatisPlusConfig.java
@@ -1,22 +1,22 @@
-//package com.kindergarten.manage.system.config;
-//
-//import com.baomidou.mybatisplus.annotation.DbType;
-//import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-//import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class MybatisPlusConfig {
-//    /**
-//     * 配置分页插件
-//     */
-//    @Bean
-//    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-//        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-//        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
-//        paginationInnerInterceptor.setOverflow(true);
-//        interceptor.addInnerInterceptor(paginationInnerInterceptor);
-//        return interceptor;
-//    }
-//}
+package com.kindergarten.basic.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 配置分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        paginationInnerInterceptor.setOverflow(true);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
+}
