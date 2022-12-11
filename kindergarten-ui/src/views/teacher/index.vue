fe1a9301d969ec5004da75b0edcfa45ad384daa6
wangruiyu <wry10150@163.com>
2022-12-11 15:11:33 +0800

    feat:完善老师管理界面操作

--- kindergarten-ui/src/views/teacher/index.vue
+++ kindergarten-ui/src/views/teacher/index.vue
@@ -72,8 +72,8 @@
       </el-table-column>
     </el-table>
     <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
+      @size-change="val=>pageInfo.pageNum=val"
+      @current-change="val=>pageInfo.pageSize=val"
       :current-page="pageInfo.pageNum"
       :page-size="pageInfo.pageSize"
       layout="total,sizes, jumper, -> ,prev, pager, next"
@@ -87,17 +87,36 @@
       width="50%"
       :before-close="handleClose"
     >
-      <span>这是一段信息</span>
+      <el-form ref="teacherForm" :model="teacherForm" label-width="80px">
+        <el-form-item label="姓名">
+          <el-input v-model="teacherForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄">
+          <el-input v-model="teacherForm.age"></el-input>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-select v-model="teacherForm.gender" placeholder="性别" style="width: 100%">
+            <el-option label="男" value="1"></el-option>
+            <el-option label="女" value="2"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="爱好">
+          <el-input v-model="teacherForm.hobbyTag"></el-input>
+        </el-form-item>
+        <el-form-item label="个人简介">
+          <el-input type="textarea" v-model="teacherForm.biography"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button @click="handleCancel">取 消</el-button>
+        <el-button type="primary" @click="handleSubmit = false">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { getList, remove } from '@/api/teacher'
+import { getList, remove, save, modify } from '@/api/teacher'
 import { Message } from 'element-ui'
 
 export default {
@@ -120,7 +139,15 @@ export default {
         user: '',
         region: ''
       },
-      dialogVisible: false
+      teacherForm: {
+        name: null,
+        age: null,
+        gender: null,
+        hobbyTag: null,
+        biography: null
+      },
+      dialogVisible: false,
+      isUpdate: false
     }
   },
   created() {
@@ -135,12 +162,6 @@ export default {
         this.listLoading = false
       })
     },
-    handleSizeChange(val) {
-      console.log(`每页 ${val} 条`)
-    },
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`)
-    },
     onSubmit() {
       this.fetchData()
     },
@@ -148,7 +169,9 @@ export default {
       this.dialogVisible = true
     },
     handleUpdate(val) {
-      console.log(val)
+      this.isUpdate = true
+      this.teacherForm = Object.assign(this.teacherForm, val)
+      this.dialogVisible = true
     },
     handleDelete(id) {
       remove({ id }).then(response => {
@@ -156,12 +179,36 @@ export default {
       })
     },
     handleClose(done) {
-      this.$confirm('确认关闭？')
-        .then(_ => {
-          done()
+      if (this.isUpdate) {
+        this.isUpdate = false
+      }
+      done()
+      this.dialogVisible = false
+    },
+    handleCancel() {
+      if (this.isUpdate) {
+        this.isUpdate = false
+      }
+      this.dialogVisible = false
+    },
+    handleSubmit() {
+      if (this.isUpdate) {
+        this.isUpdate = false
+        modify(this.teacherForm).then(() => {
+          Message.error('操作成功！')
         })
-        .catch(_ => {
+      } else {
+        save(this.teacherForm).then(() => {
+          Message.error('操作成功！')
         })
+      }
+      this.teacherForm = {
+        name: null,
+        age: null,
+        gender: null,
+        hobbyTag: null,
+        biography: null
+      }
     }
   }
 }
