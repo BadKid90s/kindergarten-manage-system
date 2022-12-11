6f109e2bcba91560b85d29b5c2d59b1b0613d886
wangruiyu <wry10150@163.com>
2022-12-11 13:41:12 +0800

    feat:添加跨域支持

--- kindergarten-manage-system/src/main/java/com/kindergarten/basic/config/WebConfiguration.java
+++ kindergarten-manage-system/src/main/java/com/kindergarten/basic/config/WebConfiguration.java
@@ -3,12 +3,22 @@ package com.kindergarten.basic.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
+                .maxAge(3600);
+    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
