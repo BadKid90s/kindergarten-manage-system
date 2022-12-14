5e17b2c0c24dee37502ef3dec66fc77ca14980e2
王瑞玉 <wangruiyu@bjknrt.com>
2022-12-14 14:39:01 +0800

    fix: 修改页面语法符合eslint

--- kindergarten-ui/src/views/teacher/index.vue
+++ kindergarten-ui/src/views/teacher/index.vue
@@ -3,17 +3,17 @@
     <el-row>
       <el-form :inline="true" :model="searchParams" class="demo-form-inline">
         <el-form-item label="姓名">
-          <el-input v-model="searchParams.name" placeholder="姓名"></el-input>
+          <el-input v-model="searchParams.name" placeholder="姓名" />
         </el-form-item>
         <el-form-item label="性别">
           <el-select v-model="searchParams.gender" placeholder="性别">
-            <el-option label="全部" value=""></el-option>
-            <el-option label="男" :value="1"></el-option>
-            <el-option label="女" :value="2"></el-option>
+            <el-option label="全部" value="" />
+            <el-option label="男" :value="1" />
+            <el-option label="女" :value="2" />
           </el-select>
         </el-form-item>
         <el-form-item label="爱好">
-          <el-input v-model="searchParams.hobbyTag" placeholder="爱好"></el-input>
+          <el-input v-model="searchParams.hobbyTag" placeholder="爱好" />
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">查询</el-button>
@@ -36,8 +36,7 @@
         label="姓名"
         width="180"
         align="center"
-      >
-      </el-table-column>
+      />
       <el-table-column
         prop="gender"
         label="性别"
@@ -53,37 +52,33 @@
         label="年龄"
         width="180"
         align="center"
-      >
-      </el-table-column>
+      />
       <el-table-column
         prop="hobbyTag"
         label="爱好"
         align="left"
-      >
-      </el-table-column>
+      />
       <el-table-column
         prop="biography"
         label="个人简介"
         align="left"
-      >
-      </el-table-column>
+      />
       <el-table-column class-name="status-col" label="Status" width="180" align="center">
         <template slot-scope="scope">
           <el-button size="small" type="warning" @click="handleUpdate(scope.row)">修改</el-button>
-          <el-button size="small" type="danger" @click="handleDelete(scope.row.id)" style="margin-left: 20px">删除
+          <el-button size="small" type="danger" style="margin-left: 20px" @click="handleDelete(scope.row.id)">删除
           </el-button>
         </template>
       </el-table-column>
     </el-table>
     <el-pagination
-      @size-change="val=>pageInfo.pageNum=val"
-      @current-change="val=>pageInfo.pageSize=val"
       :current-page="pageInfo.pageNum"
       :page-size="pageInfo.pageSize"
-      layout="total,sizes, jumper, -> ,prev, pager, next"
       :total="pageInfo.total"
-    >
-    </el-pagination>
+      layout="total,sizes, jumper, -> ,prev, pager, next"
+      @current-change="val=>pageInfo.pageSize=val"
+      @size-change="val=>pageInfo.pageNum=val"
+    />
 
     <el-dialog
       title="提示"
@@ -93,22 +88,22 @@
     >
       <el-form ref="teacherForm" :model="teacherForm" label-width="80px">
         <el-form-item label="姓名">
-          <el-input v-model="teacherForm.name"></el-input>
+          <el-input v-model="teacherForm.name" />
         </el-form-item>
         <el-form-item label="年龄">
-          <el-input v-model="teacherForm.age"></el-input>
+          <el-input v-model="teacherForm.age" />
         </el-form-item>
         <el-form-item label="性别">
           <el-select v-model="teacherForm.gender" placeholder="性别" style="width: 100%">
-            <el-option label="男" :value="1"></el-option>
-            <el-option label="女" :value="2"></el-option>
+            <el-option label="男" :value="1" />
+            <el-option label="女" :value="2" />
           </el-select>
         </el-form-item>
         <el-form-item label="爱好">
-          <el-input v-model="teacherForm.hobbyTag"></el-input>
+          <el-input v-model="teacherForm.hobbyTag" />
         </el-form-item>
         <el-form-item label="个人简介">
-          <el-input type="textarea" v-model="teacherForm.biography"></el-input>
+          <el-input v-model="teacherForm.biography" type="textarea" />
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
